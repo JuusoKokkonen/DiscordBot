7b5795e77f1e630cab4707e2d084b7a0919da4fa
Juuso Kokkonen <juusokokkonen98@gmail.com>
2020-05-04 22:29:27 +0300

    Added some test commands

--- bot.py
+++ bot.py
@@ -1,11 +1,22 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "NzA2OTMxNTE4NTk3MTAzNjQ3.XrBbnw.Ho8-50eg_fHKcQImhCXhiwDg4jM"
+TOKEN = "NzA2OTMxNTE4NTk3MTAzNjQ3.XrBoIg.5PRHcDDulAoL6M64DzXhbyP6vq4"
 client = commands.Bot(command_prefix = "!")
 
 @client.event
 async def on_ready():
     print("Bot is ready")
 
+@client.command()
+async def test(ctx):
+    await ctx.send(xd())
+
+@client.command()
+async def close(ctx):
+    await ctx.send("Shutting down")
+    await client.logout()
+
+def xd():
+    return("Testing")
 client.run(TOKEN)
